47fdc0ce224318e7045884fb855212972f4188cb
CamillePele <camille.pele.dev@gmail.com>
2023-07-07 23:53:35 +0200

    Part of save system

---
+++ Assets/Scripts/Interfaces/ISavable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface ISavable
+    {
+        public List<ISavable> Children { get; set; }
+        
+        public SimpleJSON.JSONObject Save();
+        
+        public void Load(SimpleJSON.JSONObject json);
+    }
+}
