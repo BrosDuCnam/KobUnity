f1e650a63ed745b419b1f27b99746417a6f8ebd0
CamillePele <camille.pele.dev@gmail.com>
2023-07-08 21:40:02 +0200

    Add method in ISavable

--- Assets/Scripts/Interfaces/ISavable.cs
+++ Assets/Scripts/Interfaces/ISavable.cs
@@ -7,6 +7,17 @@ namespace Interfaces
         public List<ISavable> Children { get; set; }
         
         public SimpleJSON.JSONObject Save();
+
+        public static SimpleJSON.JSONObject GetDefaultSave()
+        {
+            // Implement this in your class
+            // I need to throw an exception here because I can't make this method abstract
+#if UNITY_EDITOR
+            throw new System.NotImplementedException();
+#else
+            return null;
+#endif
+        }
         
         public void Load(SimpleJSON.JSONObject json);
     }
