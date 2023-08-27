569d29375b86f9ba642fe32dba0f223061acb959
CamillePele <camille.pele.dev@gmail.com>
2023-08-27 15:47:54 +0200

    part of inventory

---
+++ Assets/Scripts/Scriptable/ScriptableItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Scriptable
+{
+    [CreateAssetMenu(fileName = "New Scriptable Item", menuName = "Scriptable/Scriptable Item")]
+    public class ScriptableItem : ScriptableObject
+    {
+        [SerializeField] private ScriptableItem parent;
+        [SerializeField] private string id;
+        
+        [SerializeField] private string itemName;
+        [SerializeField] private Sprite icon;
+        [SerializeField] private int maxStack = 99;
+        [SerializeField] private ConsumablesData consumablesData;
+        
+        [Serializable]
+        public struct ConsumablesData
+        {
+            public int health;
+            public int thirst;
+            public int hunger;
+        }
+        
+        public void OnValidate()
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                id = Guid.NewGuid().ToString();
+                EditorUtility.SetDirty(this);
+            }
+        }
+    }
+}
